fbca12daf23a6fd1c86f81636e157c4a938744c3
Yana Slavcheva <yana.slavcheva@gmail.com>
2017-11-09 17:15:33 +0200

    Fixed quotes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var http = require("http");
-var express = require("express");
+var http = require('http');
+var express = require('express');
 var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello there')
+})
 
 var server = http.createServer(app);
 server.listen(3000);
