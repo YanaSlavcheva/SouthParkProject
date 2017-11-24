4481e362e2aaad26dd967bd0b97580764ac64a07
Yana Slavcheva <yana.slavcheva@gmail.com>
2017-11-24 22:49:42 +0200

    renaming

--- server/utilities/encryption.js
+++ server/utilities/encryption.js
@@ -3,6 +3,6 @@ const crypto = require('crypto')
 module.exports = {
     generateSalt: () =>
         crypto.randomBytes(128).toString('base64'),
-    generateHashedPassword: (salt, pwd) =>
-        crypto.createHmac('sha256', slat).update(pwd).digest('hex')
+    generateHashedPassword: (salt, password) =>
+        crypto.createHmac('sha256', salt).update(password).digest('hex')
 }
