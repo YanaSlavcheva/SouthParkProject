6749896224c9050b10f6f856b5e96f0a27a7aaed
Yana Slavcheva <yana.slavcheva@gmail.com>
2017-11-25 19:51:11 +0200

    loaded users and events from db

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,28 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
-const axios = require('axios')
-const api = 'https://jsonplaceholder.typicode.com'
-
-router.get('/users', (req, res, err) => {
-    if (err) console.log(err)
-    res.send('get users page')
+router.get('/users', (req, res) => {
+    let UserModel = require('mongoose').model('User')
+    UserModel.find().then(users => {      
+        res.json(users)
+    })
+    .catch(err => {      
+        res.status(500).send(err)
+    })
 })
 
-router.get('/events', (req, res, err) => {
-    if (err) console.log(err)
-    res.send('get events page')
-})
-
-router.post('/posts', (req, res, err) => {
-    if (err) res.status(500).send(err)
-    axios.get(`${api}/posts`)
-        .then(posts =>{
-            res.status(200).json(posts.data)
-        })
-        .catch(error => {
-            res.status(500).send(error)
-        })
+router.get('/events', (req, res) => {
+    let EventModel = require('mongoose').model('Event')
+    EventModel.find().then(events => {
+        res.json(events)
+    })
+    .catch(err=> {
+        res.status(500).send(err)
+    })
 })
 
 module.exports = router
